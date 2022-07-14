2e48ea90996ee86759587cda62528c41e051e908
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-14 16:32:30 -0400

    additional webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,26 @@ module.exports = () => {
         template: './index.html',
         title: 'Jate'
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'jate',
+        short_name: 'jate',
+        description: 'Note taker!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }), 
     ],
 
     module: {
@@ -29,6 +49,10 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
         },
         {
           test: /\.m?js$/,
